b25e740b49dd8c98dc4681b6749d395b646acdc1
Ruslan Shtybaev <79254522+Reyganstorm@users.noreply.github.com>
2023-05-28 13:12:20 +0300

    Initial Commit

---
+++ Animations in SwiftUI/Animations_in_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animations_in_SwiftUIApp.swift
+//  Animations in SwiftUI
+//
+//  Created by Руслан Штыбаев on 28.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct Animations_in_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
